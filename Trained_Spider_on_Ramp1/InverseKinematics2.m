aa114e27c9ac32019764b410b75b6b2ed9b2186b
Zaid Jaber <34957705+ZaidHJaber@users.noreply.github.com>
2024-04-26 23:22:26 +0300

    Add files via upload

---
+++ Trained_Spider_on_Ramp1/InverseKinematics2.m
@@ -0,0 +1,16 @@
+function [angles] = InverseKinematics(Xt,dh)
+        %Desired_height (dh) of the SV3 Axel 
+        dh=dh*1000;
+        Yt=80; %60
+        Z_offset = (dh+25);
+        D = sqrt((Xt)^2 + (Yt)^2);
+        d = D - 27.5;
+        R = sqrt((d)^2 + (Z_offset)^2);
+        Theta3 = -(atan( Xt/Yt )); %coxa angle
+        Theta2 =pi/2-(acos((55^2+R^2-80^2)/(2*55*R))+acos(Z_offset/R)) ; %Fumer
+        Theta1 =pi/2-(acos((55^2+80^2-R^2)/(2*55*80)));  %Tibia
+        angles = [Theta1,Theta2,Theta3];
+        
+end
+
+
